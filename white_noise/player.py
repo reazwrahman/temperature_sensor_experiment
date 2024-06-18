4aff74e451f47ff8543c91c2817a5e82a0efb3e7
Reaz Rahman <reazrahman@Reazs-Laptop.local>
2024-06-18 16:00:16 -0400

    new file

--- white_noise/player.py
+++ white_noise/player.py
@@ -5,7 +5,7 @@ import time
 pygame.mixer.init()
 
 # Load the MP3 file
-pygame.mixer.music.load('output.mp3')
+pygame.mixer.music.load('output2.mp3')
 
 # Play the MP3 file on a loop
 pygame.mixer.music.play(loops=-1)
